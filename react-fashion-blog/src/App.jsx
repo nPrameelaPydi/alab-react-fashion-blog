a90921b805170a4763254076996220ea8316a7dd
Prameela Pydi <nprameela.pydi@gmail.com>
2024-11-12 20:43:36 -0800

    use props to display different articles

--- react-fashion-blog/src/App.jsx
+++ react-fashion-blog/src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
+//import { useState } from 'react';
+
+//import PropTypes from 'prop-types';
+
 import './App.css'
 import Header from './components/Header.jsx'
 import Nav from './components/Nav.jsx'
 import Article from './components/Article.jsx'
 import Footer from './components/Footer.jsx'
+import image1 from './assets/image1.jpg'
+import image2 from './assets/image2.jpg'
 
 function App() {
 
@@ -12,8 +17,20 @@ function App() {
       <Header />
       <Nav />
       <main>
-        <Article />
-        <Article />
+        <Article
+          time="11/12/2020"
+          blogHeading="On the Street in Brooklyn"
+          imgSrc={image1}
+          content="Cray ipsum dolor sit amet consectetur adipisicing elit. Minima distinctio repellat accusantium, at officiis minus iusto labore. Velit saepe doloremque, minus exercitationem maiores optio soluta assumenda enim dolorum nemo quod perspiciatis ea porro numquam ab quas corrupti tempore quis dolore modi aut facilis et. Vero fugiat nobis, quaerat incidunt reprehenderit ut at iusto quod, id delectus possimus quibusdam ab? Dolorum consequuntur laborum eligendi rerum. Laudantium, animi omnis eligendi unde nemo architecto nisi est provident dicta distinctio incidunt, quisquam id tempore expedita cum dolorum vero temporibus sed necessitatibus quod praesentium itaque adipisci veritatis! Sint nobis quod porro, odio molestiae ab ut saepe magni quo ipsa cum est autem cumque dicta laborum ad voluptatum beatae rerum delectus aspernatur? Et ratione unde atque."
+          link="#read-more-1"
+        />
+        <Article
+          time="11/11/2020"
+          blogHeading="Vintage in Vogue"
+          imgSrc={image2}
+          content="Selfies ipsum dolor sit amet consectetur adipisicing elit. Minima distinctio repellat accusantium, at officiis minus iusto labore. Velit saepe doloremque, minus exercitationem maiores optio soluta assumenda enim dolorum nemo quod perspiciatis ea porro numquam ab quas corrupti tempore quis dolore modi aut facilis et. Vero fugiat nobis, quaerat incidunt reprehenderit ut at iusto quod, id delectus possimus quibusdam ab? Dolorum consequuntur laborum eligendi rerum. Laudantium, animi omnis eligendi unde nemo architecto nisi est provident dicta distinctio incidunt, quisquam id tempore expedita cum dolorum vero temporibus sed necessitatibus quod praesentium itaque adipisci veritatis! Sint nobis quod porro, odio molestiae ab ut saepe magni quo ipsa cum est autem cumque dicta laborum ad voluptatum beatae rerum delectus aspernatur? Et ratione unde atque."
+          link="#read-more-2"
+        />
       </main>
       <Footer />
     </>
--- react-fashion-blog/src/components/Article.jsx
+++ react-fashion-blog/src/components/Article.jsx
@@ -1,21 +1,14 @@
-import blogImage from '../assets/image1.jpg'
-import image2 from '../assets/image2.jpg'
+//import PropTypes from 'prop-types';
 
-function Article() {
+
+function Article(props) {
     return (
         <article>
-            <time datetime="11-12-20">11/12/20</time>
-            <h2>On the Street in Brooklyn</h2>
-            <img src={blogImage} alt="A woman standing on brooklyn street"></img>
-            <p>Cray ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus officiis aut mollitia,
-                laboriosam optio neque cum quos, quia ea consectetur quibusdam voluptate ex molestias rem fugit
-                dolorem soluta aspernatur dicta nulla odio eum eius quam quasi obcaecati! Eaque voluptatem tenetur
-                voluptates atque voluptate corrupti cumque blanditiis minus similique, dolor deleniti fuga natus
-                quasi ipsum, minima autem error? Veniam sapiente debitis deserunt ex aliquam reiciendis dolorum
-                autem quos labore omnis enim accusamus quia consectetur vel, repudiandae eum atque sint? Quae velit
-                nobis hic necessitatibus ipsa ea delectus laboriosam quibusdam corporis provident dignissimos natus
-                totam magnam voluptates, ab repudiandae? Maxime, ipsum odit!</p>
-            <a href="#read-more-1" class="continues">Continues...</a>
+            <time>{props.time}</time>
+            <h2>{props.blogHeading}</h2>
+            <img src={props.imgSrc} alt={props.blogHeading}></img>
+            <p>{props.content}</p>
+            <a href={props.link} className="continues">Continues...</a>
         </article>
     )
 }
